9191d75384b069d46d8d7f2864f6a4607846ef8f
WolfOfBad <mechkov.igor04@gmail.com>
2024-04-29 19:10:19 +0400

    Links microservice init

---
+++ links/src/main/kotlin/ru/wolfofbad/links/LinksApplication.kt
@@ -0,0 +1,11 @@
+package ru.wolfofbad.links
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LinksApplication
+
+fun main(args: Array<String>) {
+    runApplication<LinksApplication>(*args)
+}
